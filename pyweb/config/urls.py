fdd17c77b413899af376842cb67f482914d48afa
hwa-ryong <ocsohy@naver.com>
2023-06-21 18:29:12 +0900

    로그인 회원가입

--- pyweb/config/urls.py
+++ pyweb/config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from board import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),  #http://127.0.0.1:8000/
     path('board/', include('board.urls')),
+    path('common/', include('common.urls')),
 ]
